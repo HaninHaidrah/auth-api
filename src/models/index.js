b84044a59e429fa0a5ab91f3decc231c6b23b1d4
HaninHaidrah <haneenhadey@gmail.com>
2021-12-15 18:26:08 +0200

    lab34

--- src/models/index.js
+++ src/models/index.js
@@ -1,22 +1,25 @@
-'use strict';
+"use strict";
 
-const userModel = require('./users.js');
-const clothesModel = require('../models/clothes');
-const foodModel = require('../models/food');
-const Collection = require('../models/data-collection');
+const userModel = require("./users.js");
+const clothesModel = require("../models/clothes");
+const foodModel = require("../models/food");
+const itemModel = require("../models/items.model");
+const Collection = require("../models/data-collection");
 
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require("sequelize");
 
-const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory;';
+const DATABASE_URL = process.env.DATABASE_URL || "sqlite:memory;";
 
 const sequelize = new Sequelize(DATABASE_URL);
 
 const food = foodModel(sequelize, DataTypes);
 const clothes = clothesModel(sequelize, DataTypes);
+const items = itemModel(sequelize, DataTypes);
 
 module.exports = {
   db: sequelize,
   users: userModel(sequelize, DataTypes),
   food: new Collection(food),
   clothes: new Collection(clothes),
-}
+  items: new Collection(items),
+};
